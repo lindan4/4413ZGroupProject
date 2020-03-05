33bf4a552674f503ec69fcd290be3edddc70f7d4
Lindan Thillanayagam <lindx4@gmail.com>
2020-03-04 23:22:58 -0500

    Created connection with mySQL database. Need to discuss the setup process with the group.

--- MUGABookstore/src/ctrl/HelloSpringController.java
+++ MUGABookstore/src/ctrl/HelloSpringController.java
@@ -2,16 +2,32 @@ package ctrl;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
 @Controller
 public class HelloSpringController {
 	
-	@RequestMapping("/")
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView returnHome() {
+		String query = "select * from book";
+		
+		List<?> results = jdbcTemplate.queryForList(query);
+		System.out.println(results.toString());
+		
 		return new ModelAndView("home");
 	}
 	
