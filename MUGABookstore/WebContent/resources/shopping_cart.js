3b984a3d7512dd56ede64c3aac931c165d3bea5a
Lindan Thillanayagam <lindx4@gmail.com>
2020-03-11 12:07:56 -0400

    added spacing to buttons.

--- MUGABookstore/WebContent/resources/shopping_cart.js
+++ MUGABookstore/WebContent/resources/shopping_cart.js
@@ -1,6 +1,6 @@
 function decreaseQuantity(qtId) {
 	var count = document.getElementById(qtId).value;
-	if (count >= 0) {
+	if (count > 0) {
 		count = parseInt(count) - 1;
 		document.getElementById(qtId).value = count;
 	}
