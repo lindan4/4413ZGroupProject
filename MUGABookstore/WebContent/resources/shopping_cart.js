37969d0e541fd2b5dd37f794f982bf1cfd1328e4
Lindan Thillanayagam <lindx4@gmail.com>
2020-03-11 11:55:47 -0400

    Count can be update in shopping cart table. This needs to be pushed to session.

---
+++ MUGABookstore/WebContent/resources/shopping_cart.js
@@ -0,0 +1,21 @@
+function decreaseQuantity(qtId) {
+	var count = document.getElementById(qtId).value;
+	if (count >= 0) {
+		count = parseInt(count) - 1;
+		document.getElementById(qtId).value = count;
+	}
+	
+	
+	
+}
+
+
+function increaseQuantity(qtId) {
+	var count = document.getElementById(qtId).value;
+	if (count >= 0) {
+		count = parseInt(count) + 1;
+		document.getElementById(qtId).value = count;
+	}
+	
+	
+}
