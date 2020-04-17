8206be51b6a1493b410fa2f9ca7bf66ae60311aa
Ryan Lee <drdgvhbh@gmail.com>
2020-04-17 17:34:53 -0400

    Add Report of Books Sold Each month

--- MUGABookstore/src/ctrl/AdminController.java
+++ MUGABookstore/src/ctrl/AdminController.java
@@ -1,15 +1,61 @@
 package ctrl;
 
+import bean.BookBean;
+import dto.BooksSoldReportDTO;
+import model.BookModel;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class AdminController {
-    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    private final BookModel bookModel;
+
+    AdminController(final BookModel bookModel) {
+        this.bookModel = bookModel;
+    }
+
+
+    @GetMapping(value = "/admin")
     public ModelAndView adminDashboard() {
-        ModelAndView mv = new ModelAndView("admin_dashboard");
+        final ModelAndView mv = new ModelAndView("admin_dashboard");
+        return mv;
+    }
+
+    @PostMapping(value = "/admin")
+    public ModelAndView bookSoldReport(
+            HttpSession session,
+            HttpServletRequest request
+    ) throws ParseException {
+        final ModelAndView mv = new ModelAndView("admin_dashboard");
+        if (request.getParameter("date") != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
+            TemporalAccessor date = formatter.parse(request.getParameter("date"));
+
+            mv.addObject("reportDate", request.getParameter("date"));
+
+            final List<BookBean> books = this.bookModel.listBooksSoldDuringMonth(
+                    Year.from(date),
+                    Month.from(date)
+            );
+
+            mv.addObject("books", books);
+
+        }
 
         return mv;
     }
