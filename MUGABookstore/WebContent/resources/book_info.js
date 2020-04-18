f210edb84af5bc8babd9d6f84eb1fb0f3db58af4
Lindan Thillanayagam <lindx4@gmail.com>
2020-04-18 19:39:06 -0400

    Added validation in bookinfo page.

--- MUGABookstore/WebContent/resources/book_info.js
+++ MUGABookstore/WebContent/resources/book_info.js
@@ -1,3 +1,27 @@
 /**
  * 
  */
+
+function validateReview() {
+	var isOk = true;
+	
+	var firstStar = document.getElementById("oneStar"); 
+	var secondStar = document.getElementById("twoStar"); 
+	var thirdStar = document.getElementById("threeStar");
+	var fourthStar = document.getElementById("fourStar");
+	var fifthStar = document.getElementById("fiveStar");
+	
+	var reviewInputContent = document.getElementById("reviewInputContent");
+	
+	if (!firstStar.checked && !secondStar.checked && !thirdStar.checked && !fourStar.checked && !fifthStar.checked) {
+		isOk = false;
+		alert("Please select a rating from 1 to 5.");
+	}
+	
+	if (reviewInputContent.value == "") {
+		isOk = false;
+		alert("Please type in a review.");
+	} 
+	
+	return isOk;
+}
