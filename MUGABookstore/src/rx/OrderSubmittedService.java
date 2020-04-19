063533ac7aa3501652884934d52229b6e7a9725c
Ryan Lee <drdgvhbh@gmail.com>
2020-04-19 12:38:58 -0400

    Fix Real Time Analytics

--- MUGABookstore/src/rx/OrderSubmittedService.java
+++ MUGABookstore/src/rx/OrderSubmittedService.java
@@ -1,19 +1,18 @@
 package rx;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.core.MessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderSubmittedService implements ApplicationListener<OrderSubmittedEvent> {
-    private final MessageSendingOperations<String> messagingTemplate;
-
-    public OrderSubmittedService(MessageSendingOperations<String> messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
+    @Autowired
+    private SimpMessagingTemplate webSocket;
 
     @Override
     public void onApplicationEvent(OrderSubmittedEvent orderSubmittedEvent) {
-        this.messagingTemplate.convertAndSend("/topic/order-submitted", "order-submitted");
+        this.webSocket.convertAndSend("/order-submitted", "order-submitted");
     }
 }
--- MUGABookstore/src/rx/WebSocketConfig.java
+++ MUGABookstore/src/rx/WebSocketConfig.java
@@ -10,8 +10,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/");
         config.setApplicationDestinationPrefixes("/app");
-        config.enableSimpleBroker("/topic", "/queue");
     }
 
     @Override
